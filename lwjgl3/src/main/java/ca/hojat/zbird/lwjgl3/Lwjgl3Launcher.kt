a1287d2af319f32eb0a53e99118ff7b5385126b3
hojat <hojat72elect@gmail.com>
2024-05-08 00:38:45 -0400

    Migrated the desktop app to kotlin.

--- lwjgl3/src/main/java/ca/hojat/zbird/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/java/ca/hojat/zbird/lwjgl3/Lwjgl3Launcher.kt
@@ -1,33 +1,55 @@
-package ca.hojat.zbird.lwjgl3;
+package ca.hojat.zbird.lwjgl3
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import ca.hojat.zbird.ZombieFlappyBird
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 
 /**
  * Launches the desktop (LWJGL3) application.
  */
-public class Lwjgl3Launcher {
-    public static void main(String[] args) {
-        if (StartupHelper.startNewJvmIfRequired())
-            return; // This handles macOS support and helps on Windows.
-        createApplication();
+object Lwjgl3Launcher {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        if (StartupHelper.startNewJvmIfRequired()) return  // This handles macOS support and helps on Windows.
+
+        createApplication()
     }
 
-    private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new ca.hojat.zbird.ZombieFlappyBird(), getDefaultConfiguration());
+    private fun createApplication(): Lwjgl3Application {
+        return Lwjgl3Application(ZombieFlappyBird(), createDefaultConfiguration())
     }
 
-    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
-        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-        configuration.setTitle("ZombieBird");
-        configuration.useVsync(true);
-        //// Limits FPS to the refresh rate of the currently active monitor.
-        configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
-        //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
-        //// useful for testing performance, but can also be very stressful to some hardware.
-        //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
-        configuration.setWindowedMode(640, 480);
-        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
-        return configuration;
+    private fun createDefaultConfiguration(): Lwjgl3ApplicationConfiguration {
+
+        val windowWidth = 640
+        val windowHeight = 480
+        val icon128 = "libgdx128.png"
+        val icon64 = "libgdx64.png"
+        val icon32 = "libgdx32.png"
+        val icon16 = "libgdx16.png"
+
+        val configuration =
+            Lwjgl3ApplicationConfiguration()
+
+        configuration.setTitle("ZombieBird")
+        configuration.useVsync(true)
+        /**
+         * Limits FPS to the refresh rate of the currently active monitor.
+         *
+         * If you remove the line below and set Vsync to false, you can get unlimited FPS, which can be
+         * useful for testing performance, but can also be very stressful to some hardware.
+         * You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
+         */
+        configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate)
+        configuration.setWindowedMode(windowWidth, windowHeight)
+        configuration.setWindowIcon(
+            icon128,
+            icon64,
+            icon32,
+            icon16
+        )
+        return configuration
     }
+
+
 }
