5c1dfd5a42ca8c7e539e4e8b51cc7aa468a2c368
hojat <hojat72elect@gmail.com>
2024-05-07 23:34:17 -0400

    Migrated the Android app to kotlin.

--- android/src/main/java/ca/hojat/zbird/android/AndroidLauncher.kt
+++ android/src/main/java/ca/hojat/zbird/android/AndroidLauncher.kt
@@ -1,19 +1,18 @@
-package ca.hojat.zbird.android;
+package ca.hojat.zbird.android
 
-import android.os.Bundle;
-
-import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import android.os.Bundle
+import ca.hojat.zbird.ZombieFlappyBird
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 
 /**
  * Launches the Android application.
  */
-public class AndroidLauncher extends AndroidApplication {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
-        configuration.useImmersiveMode = true; // Recommended, but not required.
-        initialize(new ca.hojat.zbird.ZombieFlappyBird(), configuration);
+class AndroidLauncher : AndroidApplication() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val configuration = AndroidApplicationConfiguration()
+        configuration.useImmersiveMode = true // Recommended, but not required.
+        initialize(ZombieFlappyBird(), configuration)
     }
 }
