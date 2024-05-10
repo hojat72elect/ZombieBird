a00c87d112354ce022523e1dea798f94c7aaef63
hojat <hojat72elect@gmail.com>
2024-05-10 02:48:21 -0400

    SplashScreen.java -> kt

--- core/src/main/java/ca/hojat/zbird/screens/SplashScreen.kt
+++ core/src/main/java/ca/hojat/zbird/screens/SplashScreen.kt
@@ -1,103 +1,84 @@
-package ca.hojat.zbird.screens;
+package ca.hojat.zbird.screens
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import aurelienribon.tweenengine.BaseTween
+import aurelienribon.tweenengine.Tween
+import aurelienribon.tweenengine.TweenCallback
+import aurelienribon.tweenengine.TweenEquations
+import aurelienribon.tweenengine.TweenManager
+import ca.hojat.zbird.ZombieFlappyBird
+import ca.hojat.zbird.tweenaccessors.SpriteAccessor
+import ca.hojat.zbird.zbhelpers.AssetLoader
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
-import org.jetbrains.annotations.Nullable;
+class SplashScreen(private val game: ZombieFlappyBird) : Screen {
 
-import aurelienribon.tweenengine.Tween;
-import aurelienribon.tweenengine.TweenCallback;
-import aurelienribon.tweenengine.TweenEquations;
-import aurelienribon.tweenengine.TweenManager;
-import ca.hojat.zbird.ZombieFlappyBird;
-import ca.hojat.zbird.tweenaccessors.SpriteAccessor;
-import ca.hojat.zbird.zbhelpers.AssetLoader;
+    private lateinit var manager: TweenManager
+    private lateinit var batcher: SpriteBatch
+    private lateinit var sprite: Sprite
 
-public class SplashScreen implements Screen {
 
-    private final ZombieFlappyBird game;
-    @Nullable
-    private TweenManager manager;
-    @Nullable
-    private SpriteBatch batcher;
-    @Nullable
-    private Sprite sprite;
-
-    public SplashScreen(ca.hojat.zbird.ZombieFlappyBird game) {
-        this.game = game;
+    override fun show() {
+        sprite = Sprite(AssetLoader.logo)
+        sprite.setColor(1f, 1f, 1f, 0f)
+        val width = Gdx.graphics.width
+        val height = Gdx.graphics.height
+        val desiredWidth = width * .7f
+        val scale = desiredWidth / sprite.width
+        sprite.setSize(sprite.width * scale, sprite.height * scale)
+        sprite.setPosition(
+            (width / 2) - (sprite.width / 2), (height / 2)
+                    - (sprite.height / 2)
+        )
+        setupTween()
+        batcher = SpriteBatch()
     }
 
-    @Override
-    public void show() {
-        sprite = new Sprite(AssetLoader.logo);
-        sprite.setColor(1, 1, 1, 0);
-
-        float width = Gdx.graphics.getWidth();
-        float height = Gdx.graphics.getHeight();
-        float desiredWidth = width * .7f;
-        float scale = desiredWidth / sprite.getWidth();
-
-        sprite.setSize(sprite.getWidth() * scale, sprite.getHeight() * scale);
-        sprite.setPosition((width / 2) - (sprite.getWidth() / 2), (height / 2)
-                - (sprite.getHeight() / 2));
-        setupTween();
-        batcher = new SpriteBatch();
+    override fun render(delta: Float) {
+        manager.update(delta)
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        batcher.begin()
+        sprite.draw(batcher)
+        batcher.end()
     }
 
-    private void setupTween() {
-        Tween.registerAccessor(Sprite.class, new SpriteAccessor());
-        manager = new TweenManager();
-
-        TweenCallback cb = (type, source) -> game.setScreen(new ca.hojat.zbird.screens.GameScreen());
-
-        Tween.to(sprite, SpriteAccessor.ALPHA, 1.8f).target(1)
-                .ease(TweenEquations.easeInOutQuad).repeatYoyo(1, .9f)
-                .setCallback(cb).setCallbackTriggers(TweenCallback.COMPLETE)
-                .start(manager);
+    override fun resize(width: Int, height: Int) {
+        // TODO : better be implemented later
     }
 
-    @Override
-    public void render(float delta) {
-        assert manager != null;
-        manager.update(delta);
-        Gdx.gl.glClearColor(1, 1, 1, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        assert batcher != null;
-        batcher.begin();
-        assert sprite != null;
-        sprite.draw(batcher);
-        batcher.end();
+    override fun pause() {
+        //TODO
     }
 
-    @Override
-    public void resize(int width, int height) {
-
+    override fun resume() {
+        //TODO
     }
 
-    @Override
-    public void hide() {
-        // TODO Auto-generated method stub
-
+    override fun hide() {
+        //TODO
     }
 
-    @Override
-    public void pause() {
-        // TODO Auto-generated method stub
-
+    override fun dispose() {
+        //TODO
     }
 
-    @Override
-    public void resume() {
-        // TODO Auto-generated method stub
 
-    }
-
-    @Override
-    public void dispose() {
-        // TODO Auto-generated method stub
-
+    private fun setupTween() {
+        Tween.registerAccessor(Sprite::class.java, SpriteAccessor())
+        manager = TweenManager()
+        val cb = TweenCallback { _: Int, _: BaseTween<*>? ->
+            game.screen = GameScreen()
+        }
+        Tween.to(sprite, SpriteAccessor.ALPHA, 1.8f)
+            .target(1f)
+            .ease(TweenEquations.easeInOutQuad)
+            .repeatYoyo(1, .9f)
+            .setCallback(cb)
+            .setCallbackTriggers(TweenCallback.COMPLETE)
+            .start(manager)
     }
 }
